e9200d8742aeb51df07dc9903d9029e61ff658a4
NVD_ACE <nguyenvandung01082003@gmail.com>
2025-09-03 17:28:59 +0700

    Profile Page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,33 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'dvgktacwufauyqfilnyi.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
+
+// module.exports = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'assets.example.com',
+//         port: '',
+//         pathname: '/account123/**',
+//         search: '',
+//       },
+//     ],
+//   },
+// }
